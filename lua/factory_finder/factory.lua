8b2f93cfc7b84b28fc500d86024d42a2083aeb49
Amy Paslak <amy.paslak@gusto.com>
2025-03-03 11:17:34 -0600

    start utils

--- lua/factory_finder/factory.lua
+++ lua/factory_finder/factory.lua
@@ -1,4 +1,6 @@
 local M = {}
+local utils = require('factory_finder.utils')
+local shared = require("nvim-treesitter.textobjects.shared")
 
 function M.extend_treesitter()
   local factory_function_query = [[
@@ -22,27 +24,11 @@ function M.extend_treesitter()
 end
 
 function M.identify_factory_name()
-  local shared = require("nvim-treesitter.textobjects.shared")
   local bufnr, _, function_node = shared.textobject_at_point("@factory_function", "factories")
 
   if function_node then
     local query = vim.treesitter.query.parse("ruby", "(simple_symbol) @factory_name")
-
-    if query then
-      local matches = {}
-      for id, node, text_range in query:iter_captures(function_node, bufnr) do
-        table.insert(matches, { node, id, text_range })
-      end
-
-      if #matches > 0 then
-        local factory_name_node = matches[1][1]
-        local factory_name = vim.treesitter.get_node_text(factory_name_node, bufnr)
-        vim.print("Factory name:", factory_name)
-        return factory_name
-      end
-    else
-      vim.print("Query parsing failed.")
-    end
+    return utils.extract_item_name_from_query(query, function_node, bufnr)
   end
 
   return nil
