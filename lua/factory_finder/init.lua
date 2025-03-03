88e444904768bcb7d6037288d1806c5fac1992cf
Amy Paslak <amy.paslak@gusto.com>
2025-03-03 10:56:57 -0600

    v1 factory_finder with treesitter

--- lua/factory_finder/init.lua
+++ lua/factory_finder/init.lua
@@ -6,73 +6,63 @@ local default_config = {
 
 M.config = {}
 
-local utils = require('factory_finder.utils')
-local factory_finder = require('factory_finder.factory')
-local shared_example_finder = require('factory_finder.shared_example')
-local shared_context_finder = require('factory_finder.shared_context')
+local function extend_treesitter_queries()
+  local factory_function_query = [[
+    ; @factories
+    (call
+      method: (identifier) @factory_method
+      (#any-of? @factory_method
+         "create"
+         "build"
+         )
+      arguments: (argument_list
+        (simple_symbol) @factory_name
+        (pair
+          key: (hash_key_symbol)
+          value: (identifier)
+        )*
+      )
+    ) @factory_function
+  ]]
+  vim.treesitter.query.set("ruby", "factories", factory_function_query)
+
+  vim.print('prepped the queries')
+end
 
 function M.setup(user_config)
   M.config = vim.tbl_extend("force", default_config, user_config or {})
 
-  factory_finder.load_cache(M.config)
-  shared_example_finder.load_cache(M.config)
-  shared_context_finder.load_cache(M.config)
+  extend_treesitter_queries()
 end
 
--- this method relies on just the caches to be populated
-local function smart_go_to_definition(config)
-  local result
 
-  local item_name = factory_finder.parse_factory_name()
-  if item_name then
-    result = factory_finder.find_definition(item_name, config)
-    utils.open_definition(result, item_name, config)
-    return true
+local function identify_factory_name()
+  local shared = require("nvim-treesitter.textobjects.shared")
+  local bufnr, _, function_node = shared.textobject_at_point("@factory_function", "factories")
+
+  if function_node then
+    local query = vim.treesitter.query.parse("ruby", "(simple_symbol) @factory_name")
+
+    if query then
+      local matches = {}
+      for id, node, text_range in query:iter_captures(function_node, bufnr) do
+        table.insert(matches, { node, id, text_range })
+      end
+
+      if #matches > 0 then
+        local factory_name_node = matches[1][1]
+        local factory_name = vim.treesitter.get_node_text(factory_name_node, bufnr)
+        vim.print("Factory name:", factory_name)
+        return factory_name
+      end
+    else
+      vim.print("Query parsing failed.")
+    end
   end
 
-  item_name = shared_example_finder.parse_example_name()
-  if item_name then
-    result = shared_example_finder.find_definition(item_name, config)
-    utils.open_definition(result, item_name, config)
-    return true
-  end
-
-  item_name = shared_context_finder.parse_context_name()
-  if item_name then
-    result = shared_context_finder.find_definition(item_name, config)
-    utils.open_definition(result, item_name, config)
-    return true
-  end
-
-  return false
+  return nil
 end
 
-local function refresh_caches(config)
-  factory_finder.refresh_cache(config)
-  shared_example_finder.refresh_cache(config)
-  shared_context_finder.refresh_cache(config)
-end
-
-local function inspect_caches(config)
-  factory_finder.inspect_cache(config)
-  shared_example_finder.inspect_cache(config)
-  shared_context_finder.inspect_cache(config)
-end
-
-vim.api.nvim_create_user_command("SmartGoToDefinition", function() smart_go_to_definition(M.config) end, { nargs = 0, desc = "Find FactoryBot, shared_example, or shared_context definition" })
-vim.api.nvim_create_user_command("RefreshCaches", function() refresh_caches(M.config) end, { nargs = 0, desc = "Refresh all caches" })
-vim.api.nvim_create_user_command("InspectCaches", function() inspect_caches(M.config) end, { nargs = 0, desc = "Inspect all caches" })
-
-vim.api.nvim_create_user_command("FactoryGoToDefinition", function() factory_finder.go_to_definition(M.config) end, { nargs = 0, desc = "Find FactoryBot definition" })
-vim.api.nvim_create_user_command("RefreshFactoryCache", function() factory_finder.refresh_cache(M.config) end, { nargs = 0, desc = "Refresh FactoryBot cache" })
-vim.api.nvim_create_user_command("InspectFactoryCache", function() factory_finder.inspect_cache(M.config) end, { nargs = 0, desc = "Inspect FactoryBot cache" })
-
-vim.api.nvim_create_user_command("SharedExampleGoToDefinition", function() shared_example_finder.go_to_definition(M.config) end, { nargs = 0, desc = "Find shared_example definition" })
-vim.api.nvim_create_user_command("RefreshSharedExampleCache", function() shared_example_finder.refresh_cache(M.config) end, { nargs = 0, desc = "Refresh shared_example cache" })
-vim.api.nvim_create_user_command("InspectSharedExampleCache", function() shared_example_finder.inspect_cache(M.config) end, { nargs = 0, desc = "Inspect shared_example cache" })
-
-vim.api.nvim_create_user_command("SharedContextGoToDefinition", function() shared_context_finder.go_to_definition(M.config) end, { nargs = 0, desc = "Find shared_context definition" })
-vim.api.nvim_create_user_command("RefreshSharedContextCache", function() shared_context_finder.refresh_cache(M.config) end, { nargs = 0, desc = "Refresh shared_context cache" })
-vim.api.nvim_create_user_command("InspectSharedContextCache", function() shared_context_finder.inspect_cache(M.config) end, { nargs = 0, desc = "Inspect shared_context cache" })
+vim.api.nvim_create_user_command('IsFactoryFunction', function() identify_factory_name() end, {})
 
 return M
